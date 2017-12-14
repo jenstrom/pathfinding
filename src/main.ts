c7f7c80cb2cb2c00ae1de176cab4eb6b3763f5fe
Jonatan Enström <jonatan.enstrom@ad.barium.nu>
2017-12-14 16:56:50 +0100

    extended level, refactored functions, changed tile selection to on left and right click

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,33 @@ type point = {x: number, y: number};
 type node = { position: point, distanceFromStart: number, supposedDistanceFromTarget: number, parent: node }
 
 const level = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,1,0,1,1,1,1,1,1,0],
-    [0,1,1,1,1,0,1,1,1,0],
-    [0,1,1,0,0,0,0,1,1,0],
-    [0,1,1,0,1,1,1,1,1,0],
-    [0,1,0,0,0,1,1,0,1,0],
-    [0,1,0,1,1,1,1,0,1,0],
-    [0,1,0,1,0,0,0,0,1,0],
-    [0,1,1,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,0,0,1,1,0,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ];
+
+const tileColors = [
+    'green',
+    'red',
+    'yellow'
+];
+
+const directions: point[] = [
+    {x: 0, y: 1},
+    {x: 1, y: 0},
+    {x: 0, y: -1},
+    {x: -1, y: 0}
+]
 
 let start: point = {x:0, y:0};
 let target: point = {x:0, y:0};
-let settingStart = true;
 
 const setStartX = (x: number) => { start.x = x };
 const setStartY = (y: number) => { start.y = y };
@@ -25,26 +37,28 @@ const setTargetX = (x: number) => { target.x = x };
 const setTargetY = (y: number) => { target.y = y };
 
 const pathFinder = (start: point, target: point, level?: number[][]):point[] => 
-!level || pointIsValid(start, level) && pointIsValid(target, level) ?
-    getNodeAncestry([last(
-        findTarget(
+    !level || pointIsValid(start, level) && pointIsValid(target, level) ?
+    getNodeAncestry(
+        [findTarget(
             [createNodeWithTarget(start, 0, null, target)], target, level)
-        )])
-        .map(n => n.position).reverse() : [];
+        .pop()]
+    )
+    .map(n => n.position).reverse() : 
+    [];
 
 const findTarget = (openList: node[], target: point, level?: number[][], closedList:node[] = [], loops = 0): node[] => 
-    pointsAreEqual(openList[0].position, target) || loops === 1000 || !pointIsValid(openList[0].position, level) ?
+    pointsAreEqual(openList[0].position, target) || !pointIsValid(openList[0].position, level) || loops === 1000 ?
     closedList.concat(openList[0]) :
     findTarget(
         openList.concat(
-            getChildNodes(openList[0], target).filter(c => 
-                (!level || level[c.position.y][c.position.x] !== 0) &&
-                (!closedList.some(n => pointsAreEqual(n.position, c.position)) ||
-                !openList.some(n => pointsAreEqual(n.position, c.position)))
-            ))
+            getChildNodes(openList[0], target)
+            .filter(c => 
+                nodeHasValidPos(c, level) &&
+                nodePosIsNotInLists(c, [closedList, openList])
+            )
+        )
         .map(n => 
-            pointsAreAdjacent(n.position, openList[0].position) &&
-            n.parent.distanceFromStart > openList[0].distanceFromStart ?
+            currentNodeIsBetterParent(openList[0], n) ?
             createNodeWithTarget(n.position, openList[0].distanceFromStart + 1, openList[0], target) :
             n)
         .slice(1)
@@ -60,9 +74,23 @@ const getNodeAncestry = (nodes: node[]):node[] => last(nodes).parent ? getNodeAn
 
 const pointsAreAdjacent = (point1, point2) => directions.some(d => pointsAreEqual(addPoints(point1, d), point2));
 
-const pointsAreEqual = (point1: point, point2: point): boolean => point1.x === point2.x && point1.y === point2.y;
+const pointsAreEqual = (point1: point, point2: point): boolean => 
+    point1.x === point2.x && 
+    point1.y === point2.y;
 
-const pointIsValid = (point: point, level?: number[][]) => !level || !(point.x < 0) && !(point.y < 0) && !(point.x >= level[0].length) && !(point.y >= level.length) && getTileTypeOfPoint(point, level);
+const pointIsValid = (point: point, level?: number[][]) => 
+    !level || 
+    !(point.x < 0) && 
+    !(point.y < 0) && 
+    !(point.x >= level[0].length) && 
+    !(point.y >= level.length) && 
+    getTileTypeOfPoint(point, level);
+
+const pointsAreValid = (points: point[], level?: number[][]) => 
+    level && points.length ? 
+    pointIsValid(points.shift(), level) && 
+    pointsAreValid(points, level) : 
+    true;
 
 const getTileTypeOfPoint = (point: point, level: number[][]) => level[point.y][point.x];
 
@@ -88,13 +116,6 @@ const addPoints = (point1: point, point2: point): point => {
     } 
 };
 
-const directions: point[] = [
-    {x: 0, y: 1},
-    {x: 1, y: 0},
-    {x: 0, y: -1},
-    {x: -1, y: 0}
-]
-
 const getChildNodes = (node: node, target: point): node[] =>
     directions.map(d => createNodeWithTarget(
         addPoints(node.position, d),
@@ -103,33 +124,51 @@ const getChildNodes = (node: node, target: point): node[] =>
         target
     ));
 
-const tileColors = [
-    'green',
-    'red',
-    'yellow'
-];
+const nodeHasValidPos = (node: node, level: number[][]) => !level || level[node.position.y][node.position.x];
+
+const nodePosIsNotInList = (node: node, list: node[]) => !list || !list.some(n => pointsAreEqual(n.position, node.position));
+
+const nodePosIsNotInLists = (node: node, list: node[][]) => 
+    list.length ? 
+    nodePosIsNotInList(node, list[0]) && 
+    nodePosIsNotInLists(node, list.slice(1)) : 
+    true;
+
+const currentNodeIsBetterParent = (currentNode: node, potentialChild: node) => 
+    pointsAreAdjacent(currentNode.position, potentialChild.position) &&
+    potentialChild.parent.distanceFromStart > currentNode.distanceFromStart;
 
 const getElement = (id: string):HTMLElement => document.getElementById(id);
 
 const createTr = (): HTMLTableRowElement => document.createElement('tr');
-const createTd = (type: number): HTMLTableDataCellElement => {
-    let td = document.createElement('td');
-    td.classList.add(tileColors[type]);
-    return td;
-}
+const createTd = (): HTMLTableDataCellElement => document.createElement('td');
+
 const changeColor = (td: HTMLTableDataCellElement, type: number) => td.className = tileColors[type];
 const grid: HTMLTableElement = getElement('grid') as HTMLTableElement;
 
+const setStartOrTarget = (setStart: boolean, x, y) => {
+    drawGrid(grid, level);
+    if (setStart){
+        setStartX(x);
+        setStartY(y);
+    } else {
+        setTargetX(x);
+        setTargetY(y);
+    }
+    if (pointsAreValid([start, target])){
+        drawPath(pathFinder(start, target, level));
+    }
+};
 
 const drawGrid = (grid: HTMLTableElement, level: number[][]): void => {
     grid.innerHTML = '';
     level.forEach((v, i) => {
-        let tr = createTr();
-        tr.addEventListener('click', () => settingStart ? setStartY(i) : setTargetY(i));
-        let row = grid.appendChild(tr);
+        let row = grid.appendChild(createTr());
         v.forEach((c, j) => {
-            let td = createTd(c);
-            td.addEventListener('click', () => settingStart ? setStartX(j) : setTargetX(j));
+            let td = createTd();
+            td.classList.add(tileColors[c]);
+            td.addEventListener('click', () => setStartOrTarget(true, j, i));
+            td.addEventListener('contextmenu', () => setStartOrTarget(false, j, i));
             row.appendChild(td)});
     });
 }
@@ -141,23 +180,7 @@ const drawPath = (moves: point[]) => grid.children ?
         )
     ) : ''
 
-
-getElement('set').addEventListener('click', () => {
-    settingStart = !settingStart;
-    getElement('set').innerText = settingStart ? 'set end' : 'set start';
-});
-
-getElement('go').addEventListener('click', () => {
-    drawGrid(grid, level);
-    let x1: number = parseInt((getElement('x1') as HTMLInputElement).value);
-    let y1: number = parseInt((getElement('y1') as HTMLInputElement).value);
-    let x2: number = parseInt((getElement('x2') as HTMLInputElement).value);
-    let y2: number = parseInt((getElement('y2') as HTMLInputElement).value);
-    x1 && y1 && x2 && y2 ?
-    drawPath(pathFinder({x: x1, y:y1}, {x: x2, y:y2}, level)) :
-    drawPath(pathFinder(start, target, level));
-});
-
+document.addEventListener('contextmenu', event => event.preventDefault());
 drawGrid(grid, level);
 // drawPath(pathFinder({x: 1, y:1}, {x: 2, y:2}, level));
 // console.log(pathFinder({x: 1, y:1}, {x: 4, y:7}, level));
